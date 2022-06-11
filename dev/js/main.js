8f23c54e3248ba998edf0a25c7797d96dce087e0
motosasha <moto.sasha@gmail.com>
2022-06-11 23:16:51 +0300

    + main page

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,5 +1,7 @@
 //= ../../node_modules/current-device/umd/current-device.js
 //= ../../node_modules/imask/dist/imask.js
 //= ../../node_modules/swiper/swiper-bundle.js
+//= ../../node_modules/@popperjs/core/dist/umd/popper.js
+//= ../../node_modules/tippy.js/dist/tippy-bundle.umd.js
 //= ../../node_modules/smooth-scroll/dist/smooth-scroll.js
 //= partials/user.js
--- dev/js/partials/user.js
+++ dev/js/partials/user.js
@@ -3,20 +3,58 @@
 let thanks;
 
 window.onload = function() {
+	// mobile nav
 	let $body = document.body;
 	let $menuTrigger = document.querySelector('.header__menu');
-	//-let $navLinks = document.querySelectorAll('.nav .nav__links-item');
-	//-let $navCallBack = document.querySelector('.nav .connections__callback');
 	$menuTrigger.addEventListener('click', function () {
 		let bodyState = $body.getAttribute('data-state');
 		bodyState === 'open' ? $body.dataset.state = '' : $body.dataset.state = 'open'
 	});
-	// $navCallBack.addEventListener('click', function () {
-	// 	$body.dataset.state = ''
-	// });
-	// for (let i = 0; i < $navLinks.length; i++) {
-	// 	$navLinks[i].addEventListener('click', function () {
-	// 		$body.dataset.state = ''
-	// 	});
-	// }
+
+	// hero-slider
+	const heroSlider = new Swiper('.hero-slider', {
+		loop: true,
+		slidesPerView: 1,
+		spaceBetween: 20,
+		pagination: {
+			el: '.hero__slider-pagination',
+		},
+		navigation: {
+			nextEl: '.hero__slider-next',
+			prevEl: '.hero__slider-prev',
+		}
+	});
+
+	// producion-slider
+	const swiper = new Swiper('.producion-slider', {
+		loop: true,
+		slidesPerView: 'auto',
+		spaceBetween: 10,
+		centeredSlides: true,
+		breakpoints: {
+			1680: {
+				centeredSlides: false,
+				slidesPerView: 4,
+			},
+			1280: {
+				centeredSlides: false,
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			768: {
+				centeredSlides: false,
+				slidesPerView: 2,
+			}
+		},
+		pagination: {
+			el: '.producion-slider__pagination',
+		},
+		navigation: {
+			nextEl: '.producion__slider-next',
+			prevEl: '.producion__slider-prev',
+		}
+	});
+
+	// tippy.js
+	tippy('[data-tippy-content]');
 };
